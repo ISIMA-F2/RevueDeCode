85c049edd9ecabe155ccebd3b9c2c0a942a2ee46
Ayoub Nasri <nas.ayou123@gmail.com>
2024-12-17 15:49:44 +0100

    Mise a jour du style et ajout de fichier

--- src/main/java/password/AwesomePasswordChecker.java
+++ src/main/java/password/AwesomePasswordChecker.java
@@ -8,19 +8,63 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
-
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Classe qui permet de hasher un mot de pass et calculer les distances entre
- * des mots
+ * des mots.
  */
 public class AwesomePasswordChecker {
+  /**
+   * Les lettres specifiques miniscule.
+   */
+  private static final Set<Character> LOWERCASE_VOWELS = new HashSet<>();
+  /**
+   * Les lettres specifiques majuscules.
+   */
+  private static final Set<Character> UPPERCASE_VOWELS = new HashSet<>();
+  /**
+   * les lettres specifiques spéciales.
+   */
+  private static final Set<Character> SPECIAL_CHARACTERS = new HashSet<>();
 
+  static {
+    for (char c : new char[] { 'e', 's', 'a', 'i', 't', 'n', 'r', 'u', 'o', 'l'
+    }) {
+      LOWERCASE_VOWELS.add(c);
+    }
+    for (char c : new char[] { 'E', 'S', 'A', 'I', 'T', 'N', 'R', 'U', 'O', 'L'
+    }) {
+      UPPERCASE_VOWELS.add(c);
+    }
+    for (char c : new char[] { '>', '<', '-', '?', '.', '/', '!', '%', '@', '&'
+    }) {
+      SPECIAL_CHARACTERS.add(c);
+    }
+  }
+  /**
+   * La Longeur maximale d'un mot de pass.
+   */
+
+  private static final int PASSWORD_LENGTH = 28;
+  /**
+   * Nombre de byte.
+   */
+  private static final int BYTE_NUMBER = 8;
+  /**
+   * Nombre de décalage.
+   */
+  private static final int DECALAGE = 6;
+  /**
+   * L'instance Initiale.
+   */
   private static AwesomePasswordChecker instance;
-
-  // Liste des centres de clusters (représentés sous forme de tableaux de doubles)
+  /**
+   * Liste des clusters sous formes de tableaux.
+   */
   private final List<double[]> clusterCenters = new ArrayList<>();
 
   /**
@@ -28,17 +72,40 @@ public class AwesomePasswordChecker {
    */
   public AwesomePasswordChecker() {
   }
-
   /**
-   * Retourne une instance unique de la classe AwesomePasswordChecker en utilisant
-   * un fichier spécifié.
-   * 
+   * Constructeur privé qui charge les centres de clusters depuis un flux
+   * d'entrée.
+   *
+   * @param is Le flux d'entrée contenant les données des centres de clusters.
+   * @throws IOException Si une erreur de lecture se produit.
+   */
+
+  private AwesomePasswordChecker(final InputStream is) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(is));
+    String line;
+    while ((line = br.readLine()) != null) {
+      String[] values = line.split(";");
+      double[] center = new double[values.length];
+
+      for (int i = 0; i < values.length; ++i) {
+        center[i] = Double.parseDouble(values[i]);
+      }
+      clusterCenters.add(center);
+    }
+    br.close();
+  }
+  /**
+   * Retourne une instance unique de la classe AwesomePasswordChecker
+   * en utilisant un fichier spécifié.
+   *
    * @param file Le fichier contenant les centres de clusters à charger.
    * @return L'instance unique de AwesomePasswordChecker.
    * @throws IOException Si le fichier n'est pas accessible ou si une erreur de
    *                     lecture survient.
    */
-  public static AwesomePasswordChecker getInstance(File file) throws IOException {
+  
+  public static AwesomePasswordChecker getInstance(final File file)
+      throws IOException {
     if (instance == null) {
       instance = new AwesomePasswordChecker(new FileInputStream(file));
     }
@@ -46,21 +113,12 @@ public class AwesomePasswordChecker {
   }
 
   /**
-   * Retourne la liste des centres de clusters.
-   * 
-   * @return La liste des centres de clusters.
-   */
-  public List<double[]> getCluster() {
-    return clusterCenters;
-  }
-
-  /**
    * Retourne l'instance unique de la classe AwesomePasswordChecker en utilisant
    * une ressource interne.
-   * 
+   *
    * @return L'instance unique de AwesomePasswordChecker.
-   * @throws IOException Si la ressource est introuvable ou une erreur de lecture
-   *                     survient.
+   * @throws IOException Si la ressource est introuvable
+   *                     ou une erreur de lecture survient.
    */
   public static AwesomePasswordChecker getInstance() throws IOException {
     if (instance == null) {
@@ -75,87 +133,33 @@ public class AwesomePasswordChecker {
   }
 
   /**
-   * Constructeur privé qui charge les centres de clusters depuis un flux
-   * d'entrée.
-   * 
-   * @param is Le flux d'entrée contenant les données des centres de clusters.
-   * @throws IOException Si une erreur de lecture se produit.
-   */
-  private AwesomePasswordChecker(InputStream is) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(is));
-    String line;
-    while ((line = br.readLine()) != null) {
-      String[] values = line.split(";");
-      double[] center = new double[values.length];
-
-      for (int i = 0; i < values.length; ++i) {
-        center[i] = Double.parseDouble(values[i]);
-      }
-      clusterCenters.add(center);
-    }
-    br.close();
-  }
-
-  /**
    * Génère un tableau de masques à partir d'un mot de passe donné.
    * Ce masque est utilisé pour analyser la composition du mot de passe.
-   * 
+   *
    * @param password Le mot de passe à analyser.
    * @return Un tableau d'entiers représentant le masque du mot de passe.
    */
-  public int[] maskAff(String password) {
-    int[] maskArray = new int[28];
-    int limit = Math.min(password.length(), 28);
+  public int[] maskAff(final String password) {
+    int[] maskArray = new int[PASSWORD_LENGTH];
+    int limit = Math.min(password.length(), PASSWORD_LENGTH);
 
     for (int i = 0; i < limit; ++i) {
       char c = password.charAt(i);
-      switch (c) {
-        case 'e':
-        case 's':
-        case 'a':
-        case 'i':
-        case 't':
-        case 'n':
-        case 'r':
-        case 'u':
-        case 'o':
-        case 'l':
-          maskArray[i] = 1;
-          break;
-        case 'E':
-        case 'S':
-        case 'A':
-        case 'I':
-        case 'T':
-        case 'N':
-        case 'R':
-        case 'U':
-        case 'O':
-        case 'L':
-          maskArray[i] = 3;
-          break;
-        case '>':
-        case '<':
-        case '-':
-        case '?':
-        case '.':
-        case '/':
-        case '!':
-        case '%':
-        case '@':
-        case '&':
-          maskArray[i] = 6;
-          break;
-        default:
-          if (Character.isLowerCase(c)) {
-            maskArray[i] = 2;
-          } else if (Character.isUpperCase(c)) {
-            maskArray[i] = 4;
-          } else if (Character.isDigit(c)) {
-            maskArray[i] = 5;
-          } else {
-            maskArray[i] = 7;
-          }
+
+      if (LOWERCASE_VOWELS.contains(c)) {
+        maskArray[i] = 1;
+      } else if (UPPERCASE_VOWELS.contains(c)) {
+        maskArray[i] = 3;
+      } else if (SPECIAL_CHARACTERS.contains(c)) {
+        maskArray[i] = 6;
+      } else if (Character.isLowerCase(c)) {
+        maskArray[i] = 2;
+      } else if (Character.isUpperCase(c)) {
+        maskArray[i] = 4;
+      } else if (Character.isDigit(c)) {
+        maskArray[i] = 5;
+      } else {
+        maskArray[i] = 7;
       }
     }
     return maskArray;
@@ -164,12 +168,12 @@ public class AwesomePasswordChecker {
   /**
    * Calcule la distance euclidienne entre un mot de passe et les centres de
    * clusters.
-   * 
+   *
    * @param password Le mot de passe à analyser.
-   * @return La distance euclidienne minimale entre le mot de passe et les centres
-   *         de clusters.
+   * @return La distance euclidienne minimale entre le mot de passe
+   *         et les centres de clusters.
    */
-  public double getDistance(String password) {
+  public double getDistance(final String password) {
     int[] maskArray = maskAff(password);
     double minDistance = Double.MAX_VALUE;
     for (double[] center : clusterCenters) {
@@ -180,12 +184,12 @@ public class AwesomePasswordChecker {
 
   /**
    * Calcule la distance euclidienne entre deux tableaux de valeurs.
-   * 
+   *
    * @param a Le premier tableau.
    * @param b Le second tableau.
    * @return La distance euclidienne entre les deux tableaux.
    */
-  private double euclideanDistance(int[] a, double[] b) {
+  private double euclideanDistance(final int[] a, final double[] b) {
     double sum = 0;
     for (int i = 0; i < a.length; i++) {
       sum += (a[i] - b[i]) * (a[i] - b[i]);
@@ -195,21 +199,22 @@ public class AwesomePasswordChecker {
 
   /**
    * Calcule le hachage MD5 d'une chaîne de caractères donnée.
-   * 
+   *
    * @param input La chaîne de caractères à hacher.
    * @return Le hachage MD5 sous forme de chaîne hexadécimale.
    */
-  public static String ComputeMD5(String input) {
+  public static String computeMd5(final String input) {
     byte[] message = input.getBytes();
     int messageLenBytes = message.length;
 
-    int numBlocks = ((messageLenBytes + 8) >>> 6) + 1;
-    int totalLen = numBlocks << 6;
+    int numBlocks = ((messageLenBytes + BYTE_NUMBER) >>> DECALAGE) + 1;
+    int totalLen = numBlocks << DECALAGE;
     byte[] paddingBytes = new byte[totalLen - messageLenBytes];
     paddingBytes[0] = (byte) 0x80;
 
     long messageLenBits = (long) messageLenBytes << 3;
-    ByteBuffer lengthBuffer = ByteBuffer.allocate(8).order(ByteOrder.LITTLE_ENDIAN).putLong(messageLenBits);
+    ByteBuffer lengthBuffer = ByteBuffer.allocate(8).order(ByteOrder.LITTLE_ENDIAN)
+        .putLong(messageLenBits);
     byte[] lengthBytes = lengthBuffer.array();
 
     byte[] paddedMessage = new byte[totalLen];
@@ -225,14 +230,17 @@ public class AwesomePasswordChecker {
     };
 
     int[] k = {
-        0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee, 0xf57c0faf, 0x4787c62a, 0xa8304613, 0xfd469501,
-        0x698098d8, 0x8b44f7af, 0xffff5bb1, 0x895cd7be, 0x6b901122, 0xfd987193, 0xa679438e, 0x49b40821,
-        0xf61e2562, 0xc040b340, 0x265e5a51, 0xe9b6c7aa, 0xd62f105d, 0x02441453, 0xd8a1e681, 0xe7d3fbc8,
-        0x21e1cde6, 0xc33707d6, 0xf4d50d87, 0x455a14ed, 0xa9e3e905, 0xfcefa3f8, 0x676f02d9, 0x8d2a4c8a,
-        0xfffa3942, 0x8771f681, 0x6d9d6122, 0xfde5380c, 0xa4beea44, 0x4bdecfa9, 0xf6bb4b60, 0xbebfbc70,
-        0x289b7ec6, 0xeaa127fa, 0xd4ef3085, 0x04881d05, 0xd9d4d039, 0xe6db99e5, 0x1fa27cf8, 0xc4ac5665,
-        0xf4292244, 0x432aff97, 0xab9423a7, 0xfc93a039, 0x655b59c3, 0x8f0ccc92, 0xffeff47d, 0x85845dd1,
-        0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1, 0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391
+        0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee, 0xf57c0faf, 0x4787c62a,
+        0xa8304613, 0xfd469501, 0x698098d8, 0x8b44f7af, 0xffff5bb1, 0x895cd7be,
+        0x6b901122, 0xfd987193, 0xa679438e, 0x49b40821, 0xf61e2562, 0xc040b340,
+        0x265e5a51, 0xe9b6c7aa, 0xd62f105d, 0x02441453, 0xd8a1e681, 0xe7d3fbc8,
+        0x21e1cde6, 0xc33707d6, 0xf4d50d87, 0x455a14ed, 0xa9e3e905, 0xfcefa3f8,
+        0x676f02d9, 0x8d2a4c8a, 0xfffa3942, 0x8771f681, 0x6d9d6122, 0xfde5380c,
+        0xa4beea44, 0x4bdecfa9, 0xf6bb4b60, 0xbebfbc70, 0x289b7ec6, 0xeaa127fa,
+        0xd4ef3085, 0x04881d05, 0xd9d4d039, 0xe6db99e5, 0x1fa27cf8, 0xc4ac5665,
+        0xf4292244, 0x432aff97, 0xab9423a7, 0xfc93a039, 0x655b59c3, 0x8f0ccc92,
+        0xffeff47d, 0x85845dd1, 0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1,
+        0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391
     };
 
     int[] r = {
@@ -245,7 +253,8 @@ public class AwesomePasswordChecker {
     for (int i = 0; i < numBlocks; i++) {
       int[] w = new int[16];
       for (int j = 0; j < 16; j++) {
-        w[j] = ByteBuffer.wrap(paddedMessage, (i << 6) + (j << 2), 4).order(ByteOrder.LITTLE_ENDIAN).getInt();
+        w[j] = ByteBuffer.wrap(paddedMessage, (i << 6) + (j << 2), 4)
+            .order(ByteOrder.LITTLE_ENDIAN).getInt();
       }
 
       int a = h[0];
@@ -254,7 +263,8 @@ public class AwesomePasswordChecker {
       int d = h[3];
 
       for (int j = 0; j < 64; j++) {
-        int f, g;
+        int f;
+        int g;
         if (j < 16) {
           f = (b & c) | (~b & d);
           g = j;
@@ -268,7 +278,7 @@ public class AwesomePasswordChecker {
           f = c ^ (b | ~d);
           g = (7 * j) % 16;
         }
-        int temp = d;
+        final int temp = d;
         d = c;
         c = b;
         b = b + Integer.rotateLeft(a + f + k[j] + w[g], r[j]);
--- src/test/java/password/AwesomePasswordCheckerTest.java
+++ src/test/java/password/AwesomePasswordCheckerTest.java
@@ -1,6 +1,5 @@
 package password;
 
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -8,73 +7,82 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.File;
 import java.io.IOException;
+import org.junit.jupiter.api.Test;
 
+/**
+ * Classe des tests pour AwesomePasswordChecker.
+ */
 public class AwesomePasswordCheckerTest {
-
-    @Test
+  @Test
     public void getInstanceTest() throws IOException {
-        // Assurer que l'instance n'est pas null
-        assertNotNull(AwesomePasswordChecker.getInstance(), "L'instance doit etre non null");
-    }
+    // Assurer que l'instance n'est pas null
+    assertNotNull(AwesomePasswordChecker.getInstance(), "L'instance doit etre non null");
+  }
 
-    @Test
+  @Test
     public void getInstanceWihtParam() throws IOException {
-        File f = new File("src\\main\\resources\\Files\\cluster_centers_HAC_aff.csv");
-        assertNotNull(AwesomePasswordChecker.getInstance(f), "L'instance doit etre non null");
-    }
+    File f = new File("src\\main\\resources\\Files\\cluster_centers_HAC_aff.csv");
+    assertNotNull(AwesomePasswordChecker.getInstance(f), "L'instance doit etre non null");
+    File tempFile = File.createTempFile("1.987", ".txt");
+    AwesomePasswordChecker result = AwesomePasswordChecker.getInstance(tempFile);
+    assertNotNull(result);
+    tempFile.delete();
+  }
 
-    @Test
+  @Test
     public void maskAffTest() {
-        String a = "Na ";
-        AwesomePasswordChecker checker = new AwesomePasswordChecker();
-        int tab[] = checker.maskAff(a);
-        assertNotNull(tab);
-        assertTrue(tab.length == 28, "La taille doit etre 28");
-        // Verifier que le tableau correspond bien à la fonctionalité de la fonction
-        // maskAff
-        assertArrayEquals(
-                new int[] { 3, 1, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, tab);
-    }
+    String a = "Na ><zZ9";
+    AwesomePasswordChecker checker = new AwesomePasswordChecker();
+    int[] tab = checker.maskAff(a);
+    assertNotNull(tab);
+    assertTrue(tab.length == 28, "La taille doit etre 28");
+    // Verifier que le tableau correspond bien à la fonctionalité de la fonction
+    // maskAff
+    assertArrayEquals(
+                new int[] { 3, 1, 7, 6, 6, 2, 4, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, tab);
+  }
 
-    @Test
+  @Test
     public void getDistance() throws IOException {
-        String test1 = "Na";
-        double dist1 = AwesomePasswordChecker.getInstance().getDistance(test1);
-        assertNotNull(dist1, "Avoir un resultat non numeral");
-        // Assurer que la distance est une valeur numérique positive
-        assertTrue(dist1 >= 0);
-        assertEquals(4.63391363622807, dist1);
-        // Tester avec le cas d'une chaine null
-        String test2 = "";
-        double dist2 = AwesomePasswordChecker.getInstance().getDistance(test2);
-        assertTrue(dist2 == 5.073797971765403);
-    }
+    String test1 = "Na";
+    double dist1 = AwesomePasswordChecker.getInstance().getDistance(test1);
+    assertNotNull(dist1, "Avoir un resultat non numeral");
+    // Assurer que la distance est une valeur numérique positive
+    assertTrue(dist1 >= 0);
+    assertEquals(4.63391363622807, dist1);
+    // Tester avec le cas d'une chaine null
+    String test2 = "";
+    double dist2 = AwesomePasswordChecker.getInstance().getDistance(test2);
+    assertTrue(dist2 == 5.073797971765403);
+  }
 
-    @Test
-    public void MD5HasherTest() {
-        // Definir les test
-        String motCourt = "Nasri";
-        String motLong = "fghfbshfdyfgsbekhfse";
-        String motCaractereSpecial = "&àçà&)àé&ééàé$ù$$£%£";
-        // Tester chaque cas
-        testDePerformance(motCourt, "Mot court");
-        testDePerformance(motLong, "Mot long");
-        testDePerformance(motCaractereSpecial, "Mot avec des caractères spéciaux");
+  @Test
+    public void md5HasherTest() {
+    // Definir les test
+    String motCourt = "Nasri";
+    String motLong = "fghfbshfdyfgsbekhfse";
+    String motCaractereSpecial = "&àçà&)àé&ééàé$ù$$£%£";
+    // Tester chaque cas
+    testDePerformance(motCourt, "Mot court");
+    testDePerformance(motLong, "Mot long");
+    testDePerformance(motCaractereSpecial,
+        "Mot avec des caractères spéciaux");
 
-    }
+  }
 
-    private void testDePerformance(String input, String description) {
-        int iterations = 1000;
-        long tempsTotal = 0;
-        for (int i = 0; i < iterations; i++) {
-            long debut = System.nanoTime();
-            String result = AwesomePasswordChecker.ComputeMD5(input);
-            long fin = System.nanoTime();
-            tempsTotal += (fin - debut);
-            assertNotNull(result, "Le résultat ne doit pas etre null");
-        }
-        double tempsMoyen = tempsTotal / (double) iterations;
-        System.out.printf("%s: Temps moyen d'executions est :  %.2f nanoseconds (%d iterations)%n",
-                description, tempsMoyen, iterations);
+  private void testDePerformance(String input, String description) {
+    int iterations = 1000;
+    long tempsTotal = 0;
+    for (int i = 0; i < iterations; i++) {
+      long debut = System.nanoTime();
+      String result = AwesomePasswordChecker.computeMd5(input);
+      long fin = System.nanoTime();
+      tempsTotal += (fin - debut);
+      assertNotNull(result, "Le résultat ne doit pas etre null");
     }
+    double tempsMoyen = tempsTotal / (double) iterations;
+    System.out.printf("%s: Temps moyen d'executions est :  %.2f nanoseconds (%d iterations)%n",
+            description, tempsMoyen, iterations);
+  }
 }
